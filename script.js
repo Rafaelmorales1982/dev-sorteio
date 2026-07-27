ab4bb00be2fea28d13a9e7ff82267ff4346c104d
Rafaelmorales1982 <rafaelmorconcursos@gmail.com>
2026-07-27 19:16:38 -0300

    Projeto dev sorteio linguagens HTML, CSS e Javascript

---
+++ script.js
@@ -0,0 +1,19 @@
+const inputMin = document.querySelector(".input-min");
+const inputMax = document.querySelector(".input-max");
+const resultado = document.querySelector(".resultado");
+const randomizarBtn = document.querySelector(".randomizar-btn");
+
+
+function sortear(){
+    const minimo = Number(inputMin.value);
+    const maximo = Number(inputMax.value);
+
+    const numeroSorteado = Math.floor(Math.random() * (maximo - minimo + 1)) + minimo;
+
+   resultado.innerHTML = numeroSorteado;
+
+}
+
+
+randomizarBtn.addEventListener("click", sortear);
+
