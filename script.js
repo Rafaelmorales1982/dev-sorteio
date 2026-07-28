e5efcdd2ca0a5964e5ee690fdfdd46c248d4d28a
Rafaelmorales1982 <rafaelmorconcursos@gmail.com>
2026-07-27 22:36:43 -0300

    acrescentando function mudar imagem utilizando array e setInterval

--- script.js
+++ script.js
@@ -2,19 +2,52 @@ const inputMin = document.querySelector(".input-min");
 const inputMax = document.querySelector(".input-max");
 const resultado = document.querySelector(".resultado");
 const randomizarBtn = document.querySelector(".randomizar-btn");
+const imagem = document.querySelector("#imagem");
 
 
-function sortear(){
-    const minimo = Number(inputMin.value);
-    const maximo = Number(inputMax.value);
+/*Array de imagens */
+const imagens = [
+   "./img/titulo-sorteio.png",
+   "./img/titulo-sorteio2.png",
+   "./img/titulo-sorteio3.png",
+];
 
-    const numeroSorteado = Math.floor(Math.random() * (maximo - minimo + 1)) + minimo;
+/*Função para mudar imagem cada 3 segundos */
+let contador = 0;
+function mudarImagem(){
+    
+    imagem.src = imagens[contador];
+    contador++;
 
-   resultado.innerHTML = numeroSorteado;
+     // volta para a primeira imagem quando chegar no final
+    if(contador >= imagens.length){
+        contador = 0;
+    }
+}setInterval(mudarImagem,2000);// muda a imagem a cada 2 segundos
 
+//-----------------------------------------
+
+
+
+function sortear() {
+  const minimo = Number(inputMin.value);
+  const maximo = Number(inputMax.value);
+
+  if (minimo > maximo) {
+    alert("O valor mínimo tem que ser MENOR que o valor MÁXIMO");
+
+    // location.reload();
+
+    inputMin.value = "";
+    inputMax.value = "";
+    resultado.textContent = "";
+    inputMin.focus();
+  } else {
+    const numeroSorteado =
+      Math.floor(Math.random() * (maximo - minimo + 1)) + minimo;
+
+    resultado.textContent = numeroSorteado;
+  }
 }
 
-
 randomizarBtn.addEventListener("click", sortear);
-
-
